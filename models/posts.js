f71d19d9d49fc5874da49674588608c2005a7066
kvtrice <katricemountford@gmail.com>
2024-02-16 15:00:05 +1100

    Started post model

---
+++ models/posts.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose"
+
+// Posts Schema
+const postsSchema = new mongoose.Schema({
+    createdDateTime: { type: Date, required: true },
+    user: { type: mongoose.ObjectId, ref: 'User' },
+    title: { type: String, required: true },
+    content: {type: String, required: true },
+    image: { type: String },
+    parent: { type: mongoose.ObjectId, ref: 'Post'},
+    isThreadStarter: { type: Boolean, required: true },
+    isComment: { type: Boolean, required: true },
+    tags: { type: Array }
+})
+
+// Post Model
+const PostModel = mongoose.model('Post', postsSchema)
+
+
+export { PostModel }
