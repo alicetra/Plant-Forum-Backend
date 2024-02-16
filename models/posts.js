d3d228433708f9ad60531cb879c4347ec5bcfb77
kvtrice <katricemountford@gmail.com>
2024-02-16 16:03:12 +1100

    Added plants to tags in postModel

--- models/posts.js
+++ models/posts.js
@@ -1,20 +1,38 @@
 import mongoose from "mongoose"
+import { plants } from "models/users.js"
 
 // Posts Schema
 const postsSchema = new mongoose.Schema({
-    createdDateTime: { type: Date, required: true },
-    user: { type: mongoose.ObjectId, ref: 'User' },
-    title: { type: String, required: true },
-    content: {type: String, required: true },
-    image: { type: String },
-    parent: { type: mongoose.ObjectId, ref: 'Post'},
-    isThreadStarter: { type: Boolean, required: true },
-    isComment: { type: Boolean, required: true },
-    tags: { type: Array }
+    createdDateTime: { 
+        type: Date, 
+        required: true },
+    userID: { 
+        type: mongoose.ObjectId, 
+        ref: 'User' },
+    title: { 
+        type: String, 
+        required: true },
+    content: {
+        type: String, 
+        required: true },
+    image: { 
+        type: String },
+    parent: { 
+        type: mongoose.ObjectId, 
+        ref: 'Post'},
+    isThreadStarter: { 
+        type: Boolean, 
+        required: true },
+    isComment: { 
+        type: Boolean, 
+        required: true },
+    tags: { 
+        type: [String],
+        required: true, 
+        enum: plants },
 })
 
 // Post Model
 const PostModel = mongoose.model('Post', postsSchema)
-
 
 export default PostModel 
