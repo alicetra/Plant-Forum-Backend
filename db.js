89f92ee492a4d6b9b7bb597eb8f1c9fed388df14
kvtrice <katricemountford@gmail.com>
2024-02-16 17:05:21 +1100

    Added post model and tested successful seed

--- db.js
+++ db.js
@@ -6,7 +6,6 @@ dotenv.config()
 // Wrapping it in a function so I can use it in the seed.js
 const connectToDatabase = async () => {
   try {
-      // put mongoose connection as early as you can even before the express
       const m = await mongoose.connect(process.env.DB_URI);
       console.log(m.connection.readyState === 1 ? 'MongoDB connected!' : 'MongoDB failed to connect');
     } catch (err) {
--- seed.js
+++ seed.js
@@ -1,33 +1,63 @@
 import UserModel from './models/users.js'
-import PostModel from './models/post.js'
+import PostModel from './models/posts.js'
 import { connectToDatabase, closeConnection } from './db.js'
 
+// Connect to database
+connectToDatabase()
+
+// Define new users to seed
 const users = [
     {
-        username: "test1",
-        password: "test1234",
+        username: "sarahxxx",
+        password: "password2",
         plants: ["Monstera", "ZZ plant"],
         profilePicture: "https://cdn.shopify.com/s/files/1/0997/4496/articles/shutterstock_597959525_07dcd775-31fc-4a78-b42a-4fffa0febd35.jpg?v=1588963145"
     },
     {
-        username: "test2",
-        password: "test1234",
+        username: "MichaelBarry",
+        password: "password2",
         plants: ["ZZ plant"],
         profilePicture: "https://images.ctfassets.net/sfnkq8lmu5d7/3g9BQo5pU4OOvi6nNugN4n/552676f869d72f164ed20302995fb93c/2021_03-catSafePlants-AdobeStock_314908743.jpg?w=1000&h=750&q=70&fm=webp"
     }
 ]
 
-const seedUsers = async () => {
-    try {
-        await UserModel.deleteMany()
-        console.log('Deleted existing users')
-        await UserModel.insertMany(users) 
-        console.log('Added users')
-    } catch (error) {
-        console.error('Error creating user:', error);
-    }
-}
+// Delete any existing users
+await UserModel.deleteMany()
+console.log('Deleted existing users')
 
-connectToDatabase()
-seedUsers()
-closeConnection()
+// Seed new users
+const newUsers = await UserModel.insertMany(users) 
+console.log('Added users')
+
+// Define posts to seed
+const posts = [
+    {
+        user: newUsers[1]._id,
+        title: "How to test fertiliser?",
+        content: "What tools does everyone use to test fertiliser, I need some recommendations pls",
+        isThreadStarter: false,
+        isComment: false,
+        tags: ["Monstera"]
+    },
+    {
+        user: newUsers[0]._id,
+        title: "How can I save this leaf?",
+        content: "I don't understand why it's dying - help!!",
+        image: "https://images.pexels.com/photos/1407305/pexels-photo-1407305.jpeg",
+        isThreadStarter: true,
+        isComment: true,
+        tags: ["Monstera"]
+    }
+]
+
+// Delete any existing posts
+await PostModel.deleteMany()
+console.log('Deleted existing posts')
+
+
+// Seed new posts
+await PostModel.insertMany(posts)
+console.log('Inserted Posts')
+
+// Close the db connection
+closeConnection();
