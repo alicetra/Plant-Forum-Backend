8a1b9cf96f371aa4ff90a084a25e9c1ff3c88679
alicetra <alicetra0109@gmail.com>
2024-02-19 10:58:56 +1100

    tested the route it works now

--- routes/get_routes.js
+++ routes/get_routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express"
 import PostModel from '../models/posts.js'
+import { connectToDatabase } from '../db.js'
 
-
+connectToDatabase()
 const router = Router()
 
 router.get('/:id', async (req, res) => {
@@ -11,7 +12,7 @@ router.get('/:id', async (req, res) => {
         // Express captures “60d5ecf31f4e5c5508fe8b2a” and stores it in req.params under the key “id”
         // since req.params.id is “60d5ecf31f4e5c5508fe8b2a”, findById(req.params.id) is equivalent to findById("60d5ecf31f4e5c5508fe8b2a"),
         // which tells mongoose to find one document in the Posts collection where the _id field is “60d5ecf31f4e5c5508fe8b2a”.
-        const entry = await PostModel.findById(req.params.id).populate('postId')
+        const entry = await PostModel.findById(req.params.id).populate('content')
         if(entry){
             res.send(entry)
         } else {
@@ -23,7 +24,7 @@ router.get('/:id', async (req, res) => {
     }
 })
 
-router.get('/', async (req, res) => res.send(await PostModel.find().populate('posts')))
+router.get('/', async (req, res) => res.send(await PostModel.find().populate('content')))
 
 
 export default router
