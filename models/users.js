6f977ba2246ed42f5a21a46673668643eafb15f2
kvtrice <katricemountford@gmail.com>
2024-02-20 10:42:08 +1100

    removed plants list from backend

--- models/users.js
+++ models/users.js
@@ -1,39 +1,5 @@
 import mongoose from 'mongoose'
 
-//setting our plants selections that user can pick from that they have 
-export const plants = [
-	"Aloe vera",
-	"Birds nest fern",
-	"Boston fern",
-	"Calathea",
-	"Cast iron plant",
-	"Chinese evergreen",
-	"Devils ivy (Pothos)",
-	"Dragon tree (Dracaena)",
-	"English Ivy",
-	"Fiddle leaf fig",
-	"Flamingo lily (Anthurium)",
-	"Jade plant",
-	"Kentia palm",
-	"Lavender",
-	"Lucky bamboo",
-	"Maidenhair fern",
-	"Money tree (Pachira aquatica)",
-	"Monstera",
-	"Orchid",
-	"Peace lily",
-	"Philodendren",
-	"Prayer plant (Maranta)",
-	"Rubber plant (Ficus elastica)",
-	"Snake plant (Sansevieria)",
-	"Spider plant",
-	"String of pearls",
-	"Swiss cheese plant (Monstera deliciosa)",
-	"Umbrella tree",
-	"Venus flytrap",
-	"ZZ plant",
-];
-
 const usersSchema = new mongoose.Schema({
     username: { 
       type : String,
@@ -57,8 +23,6 @@ const usersSchema = new mongoose.Schema({
       // insert an array of strings aka plants name
       type: [String],
       required: true,
-      //if user provide in their array a plant name that is not in the plantEnum array it throws an error
-      enum: plants,
     },
 
     profilePicture: {
